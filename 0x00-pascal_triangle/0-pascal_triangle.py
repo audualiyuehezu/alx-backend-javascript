832d36e6993015c7ecf57b704fc71bce77b17c82
audualiyuehezu <alidevopsolid@outlook.com>
2022-12-22 17:25:34 -0500

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,14 @@
 # Print Pascal's Triangle in Python
 
 # input n
-n = 5
+def pascal_triangle(n):
 
-# iterarte upto n
-for i in range(n):
-	# adjust space
-	print(' '*(n-i), end='')
+	# iterarte upto n
+	for i in range(n):
+		# adjust space
+		print(' '*(n-i), end='')
 
-	# compute power of 11
-	print(' '.join(map(str, str(11**i))))
+		# compute power of 11
+		print(' '.join(map(str, str(11**i))))
+
+pascal_triangle(7)
